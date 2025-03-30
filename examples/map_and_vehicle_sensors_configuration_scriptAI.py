190a68d4a2fea62581650ae62f20cdcf9675199f
cpap-beamng <122521243+cpap-beamng@users.noreply.github.com>
2025-03-30 10:44:23 +0200

    examples on sensors configuration and scriptAI

---
+++ examples/map_and_vehicle_sensors_configuration_scriptAI.py
@@ -0,0 +1,91 @@
+import random
+from time import sleep
+
+from beamngpy import BeamNGpy, Scenario, Vehicle, set_up_simple_logging
+from beamngpy.sensors import MapSensorConfig
+from beamngpy.sensors import VehicleSensorConfig
+
+import json
+
+# Load the JSON file      
+# add path to json file
+file_path = ".../rec1.json"
+
+def convert_json_trajectory(file_path):
+    with open(file_path, "r") as file:
+        data = json.load(file)
+    
+    # Extract the path
+    script = [
+        {
+            "t": node["t"],
+            "x": node["x"],
+            "y": node["y"],
+            "z": node["z"]
+        }
+        for node in data["path"]
+    ]
+    
+    return script
+
+# Convert the JSON trajectory
+script_format = convert_json_trajectory(file_path)
+
+def main():
+    random.seed(1703)
+    set_up_simple_logging()
+
+    beamng = BeamNGpy("localhost", 25252)
+    bng = beamng.open(launch=True)
+
+    scenario = Scenario(
+        "west_coast_usa",
+        "Map_Vehicle_Sensor_Config_Demo",
+        description="Importing a pre-made ADAS sensor suite.",
+    )
+
+    vehicle = Vehicle("ego_vehicle", model="etk800", license="ADASConfig")
+
+    scenario.add_vehicle(
+        vehicle, pos=(-725.548, 553.977, 120.262), rot_quat=(0, 0, 0.3826834, 0.9238795)
+    )
+    scenario.make(bng)
+
+    bng.settings.set_deterministic(60)  # Set simulator to 60hz temporal resolution
+
+    bng.scenario.load(scenario)
+    bng.ui.hide_hud()
+    bng.scenario.start()
+
+    vehicle.ai.set_script(script_format)
+
+    # Path to config file is relative to user folder ie: /AppData/Local/BeamNG.drive/0.XX/
+    config = MapSensorConfig("configM", bng, "/wusa.json")
+
+
+    # Path to config file is relative to user folder ie: /AppData/Local/BeamNG.drive/0.XX/
+    configV = VehicleSensorConfig("configV", bng, vehicle, "/vconfig1.json")
+
+    print("Driving around, polling all sensors of the configuration periodically...")
+    for _ in range(20):
+        sleep(1)
+        for s in range(len(config.sensors)):
+            sensor = config.sensors[s]
+            print(sensor.name)
+            print(sensor.poll())
+
+            sensorV = configV.sensors[s]
+            print(sensorV.name)
+            print(sensorV.poll())
+
+    config.remove()
+    configV.remove()
+
+    bng.ui.show_hud()
+    # vehicle.ai.set_mode("disabled")
+
+    print("Scenario finished.")
+    input("Press Enter to exit...")
+
+if __name__ == "__main__":
+    main()
