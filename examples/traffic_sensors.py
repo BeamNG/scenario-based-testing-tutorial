a76c8c24ecae9d683d1ad910c725964ed1462bdd
cpap-beamng <122521243+cpap-beamng@users.noreply.github.com>
2025-04-03 15:50:33 +0300

    Update traffic_sensors.py

--- examples/traffic_sensors.py
+++ examples/traffic_sensors.py
@@ -1,3 +1,16 @@
+"""
+How to launch a traffic scenario with sensors configuration :
+1. Open traffic_sensors.py in the IDLE (for example Visual Studio Code);
+2. Make sure that the files 
+- traffic_config_example.json
+- lanechange2.json
+- veh_sensors2.json
+are in the folder 'data'
+3. Run the script.
+"""
+
+Note : If the software version changes from 0.35 to X.XX, one should edit the version number accordingly in the traffic_sensors.py file (where commented) and at the traffic_config_example.json, line 51.
+
 import shutil
 from pathlib import Path
 from time import sleep
