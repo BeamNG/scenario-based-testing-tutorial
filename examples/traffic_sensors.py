0b746f36b8261592860ab0a830a20a4b1e930123
l-beamng <lbartali@beamng.gmbh>
2025-04-04 12:28:15 +0200

    updated traffic_sensors.py

--- examples/traffic_sensors.py
+++ examples/traffic_sensors.py
@@ -1,7 +1,7 @@
 """
 How to launch a traffic scenario with sensors configuration :
 1. Open traffic_sensors.py in the IDLE (for example Visual Studio Code);
-2. Make sure that the files 
+2. Make sure that the files
 - traffic_config_example.json
 - lanechange2.json
 - veh_sensors2.json
@@ -9,7 +9,7 @@ are in the folder 'data'
 3. Run the script.
 """
 
-Note : If the software version changes from 0.35 to X.XX, one should edit the version number accordingly in the traffic_sensors.py file (where commented) and at the traffic_config_example.json, line 51.
+#Note : If the software version changes from 0.35 to X.XX, one should edit the version number accordingly in the traffic_sensors.py file (where commented) and at the traffic_config_example.json, line 51.
 
 import shutil
 from pathlib import Path
